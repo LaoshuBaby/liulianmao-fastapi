0720b752018e68d20b85e495e20e40a5db83c21d
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-08-29 19:27:16 +0800

    fix: succeed in await call ask() and response

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI, HTTPException, Path, Request
 from fastapi.responses import FileResponse, HTMLResponse
 from liulianmao import ask
-
+from loguru import logger
 
 const_model_mapping = {
     "gpt-3-turbo": "glm-4",
@@ -14,11 +16,14 @@ const_model_mapping = {
 app = FastAPI()
 
 
-def forward_chat(request: Request):
-    body = request.json()
+async def forward_chat(request: Request):
+    # 使用 await 来获取请求体中的 JSON 数据
+    body = await request.json()
+    logger.critical(body)
     model = body.get("model")
     messages = body.get("messages")
 
+    # ask不是异步的，返回简单字符串
     ans = ask(
         msg=messages[0]["content"],
         available_models=[],
@@ -26,18 +31,21 @@ def forward_chat(request: Request):
         no_history=False,
         image_type="none",
     )
+    print(ans)
     return ans
 
 
-def forward_embedding(request: Request):
+async def forward_embedding(request: Request):
     try:
-        body = request.json()
+        # 使用 await 来获取请求体中的 JSON 数据
+        body = await request.json()
         model = body.get("model")
         input_text = body.get("input")
 
         if model in const_model_mapping:
             model = const_model_mapping[model]
 
+        # ask不是异步的，返回简单字符串
         response = ask(msg=input_text, model=model)
         return response
     except Exception as e:
@@ -46,12 +54,12 @@ def forward_embedding(request: Request):
 
 @app.get("/")
 async def hello():
-    return FileResponse("src/index.html")
+    return FileResponse(os.path.join(os.path.dirname(__file__), "index.html"))
 
 
 @app.get("/hello")
 async def hello_route():
-    return FileResponse("src/index.html")
+    return FileResponse(os.path.join(os.path.dirname(__file__), "index.html"))
 
 
 @app.post("/paas/v1/chat/completions")
@@ -61,12 +69,12 @@ async def pass_v1_chat_completions(request: Request):
 
 @app.post("/paas/v4/chat/completions")
 async def pass_v4_chat_completions(request: Request):
-    return forward_chat(request)
+    return await forward_chat(request)
 
 
 @app.post("/embedding")
 async def create_embedding(request: Request):
-    return forward_embedding(request)
+    return await forward_embedding(request)
 
 
 if __name__ == "__main__":
